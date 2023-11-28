48c18ece54ee8684f6344762d1f7b2d4d058c4b8
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-11-28 17:27:40 +0530

    added links to page

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,5 +1,14 @@
+import { Fragment } from "react";
+import Link from 'next/link';
+
 const AboutUs = () => {
-  return <h1>This is About Us Page</h1>;
+  return <Fragment><h1>This is About Us Page</h1>
+    <ul>
+      <li><Link href='/aboutus/1'>Yash</Link></li>
+      <li><Link href='/aboutus/2'>Vaibhav</Link></li>
+      <li><Link href='/aboutus/3'>Suresh</Link></li>
+    </ul>
+    </Fragment>
 };
 
 export default AboutUs;
